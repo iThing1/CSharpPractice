ea950f185175b59fccf0c04754ef59250bb81abe
iThing1 <r930324@gmail.com>
2026-03-30 16:04:19 +0900

    [update]숫자 및 연산자 선택
    -두 숫자와 연산자를 입력하는 코드
    -콘솔에서 입력받는 '숫자' 는 사실 문자이므로 문자열에서 double로 변환하는 과정이 필요함->TryParse사용
    -TryParse()을 사용하여 원하지 않는 값이 들어왔을 경우 에러메세지를 출력

--- CSharp_First/CSharp_First/Program.cs
+++ CSharp_First/CSharp_First/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace CSharp_First
+class Calculator
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
 
+        Console.Write("첫 번째 숫자를 입력하세요: ");
+        string inputFirst = Console.ReadLine();
+        if (!double.TryParse(inputFirst, out double numFirst))
+        {
+            Console.WriteLine("오류: 올바른 숫자가 아닙니다.\n");
+            return;
         }
+
+        Console.Write("연산자를 입력하세요 (+, -, *, /): ");
+        string selectedOp = Console.ReadLine();
+
+        Console.Write("두 번째 숫자를 입력하세요: ");
+        string inputSecond = Console.ReadLine() ?? "";
+        if (!double.TryParse(inputSecond, out double numSecond))
+        {
+            Console.WriteLine("오류: 올바른 숫자가 아닙니다.\n");
+            return;
+        }
+
     }
 }
