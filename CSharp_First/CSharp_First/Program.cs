e07643ab39281c235ed9a48fa81bbc5b1d2ceb82
iThing1 <r930324@gmail.com>
2026-04-01 20:12:12 +0900

    [update]출력메세지 수정&GameManager 구현
    -게임의 상태에 따라 다른 메서드를 출력하는 방법으로 설계하였다.
    -게임의 진행방식을 나누어 메서드로 각각 관리하였다.
    -InitGame: 게임 시작(객체 생성), RunGame: 게임 루프, MoveRoom: 이동로직

--- CSharp_First/CSharp_First/Program.cs
+++ CSharp_First/CSharp_First/Program.cs
@@ -78,7 +78,7 @@ namespace CSharp_First
 
         public override void Interact(Player player)
         {
-            Console.WriteLine($"[{Name}]을(를) 조사합니다.");
+            Console.WriteLine($"[System]{Name}을(를) 조사합니다.");
             if (IsLocked)
             {
                 Console.WriteLine($"{itemMgr.GetItemName(ReqItemID)}가 필요할 것 같다.");
@@ -86,7 +86,7 @@ namespace CSharp_First
                 {
                     Unlock(); 
                     player.Inventory.Remove(ReqItemID);
-                    Console.WriteLine($"[{itemMgr.GetItemName(ReqItemID)}]가 사라졌습니다.");
+                    Console.WriteLine($"[System]{itemMgr.GetItemName(ReqItemID)}이/가 사라졌습니다.");
                 }
                 else Console.WriteLine("하지만 열쇠가 없다. 다른 곳을 더 찾아보자.");
                 return;
@@ -99,7 +99,7 @@ namespace CSharp_First
             if (IsLocked)
             {
                 IsLocked = false;
-                Console.WriteLine("덜컥! 금고가 열렸습니다.");
+                Console.WriteLine("금고가 열렸다!!.");
 
                 DropItem();
             }
@@ -108,7 +108,7 @@ namespace CSharp_First
         public void DropItem()
         {
             string droppedItemName = itemMgr.GetItemName(DropItemID);
-            Console.WriteLine($"금고 안에서 [{droppedItemName}]을(를) 발견했습니다!");
+            Console.WriteLine($"금고 안에서 [{droppedItemName}]을(를) 발견했다!");
         }
     }
 
@@ -129,19 +129,42 @@ namespace CSharp_First
 
     public class GameManager
     {
+        public enum GameState {
+            None,
+            Start, 
+            Running, 
+            Exit 
+        }
+        public GameState CurrentState { get; private set; }
+
         private Room[] mRooms;
         private int mCurrentRoomIndex = 0;
         public Player Player { get; private set; }
-        public bool IsRunning { get; private set; } = true;
+        public bool IsRunning { get; private set; } = false;
 
         public GameManager()
         {
             Player = new Player();
-            InitGame();
+            CurrentState = GameState.None;
+        }
+
+        public void ChangeState(GameState state)
+        {
+            CurrentState = state;
+            switch (state)
+            {
+                case GameState.Start:
+                    InitGame(); break;
+                case GameState.Running:
+                    RunGame(); break;
+                case GameState.Exit: 
+                    ExitGame();break;
+            }
         }
 
         public void InitGame()
         {
+            Console.WriteLine("Loading......");
             mRooms = new Room[4];
             mRooms[0] = new Room("방문");
             mRooms[1] = new Room("옷장이 보이는 벽면");
@@ -149,15 +172,96 @@ namespace CSharp_First
             mRooms[3] = new Room("침대가 있는 쪽");
             
             // 여기서부터 공간안에 들어갈 물체를 생성 및 배치
-            // 예시: mRoom[1].ObjectList.Add(new SafeBox());
+            mRooms[1].objectList.Add(new SafeBox());
+
+            Console.WriteLine("게임 시작");
+            ChangeState(GameState.Running);
         }
 
-        public void MoveRoom(int dir)
+        public void RunGame()
         {
+            IsRunning = true;
+            while (IsRunning)
+            {
+                Console.Clear();
+                Console.WriteLine($"--- 현재 위치: {mRooms[mCurrentRoomIndex].Name} ---");
+                Console.WriteLine("무엇을 할까?");
+                Console.WriteLine("1. 이동  2. 조사  0. 종료");
+                if (!int.TryParse(Console.ReadLine(), out int selectNum))
+                {
+                    Console.WriteLine("숫자만 입력 가능합니다.");
+                    Console.ReadKey();
+                    continue;
+                }
+
+                switch (selectNum)
+                {
+                    case 1: // 이동 로직
+                        Console.WriteLine("1. 왼쪽  2. 오른쪽");
+                        if (int.TryParse(Console.ReadLine(), out int dirNum))
+                        {
+                            if (dirNum == 1) MoveRoom("left");
+                            else if (dirNum == 2) MoveRoom("right");
+                        }
+                        break;
+
+                    case 2: // 조사 로직
+                        var currentObjects = mRooms[mCurrentRoomIndex].objectList;
+                        if (currentObjects.Count == 0)
+                        {
+                            Console.WriteLine("여기는 조사할 만한 것이 없다.");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("눈에 들어오는 몇개의 물건이 있다.");
+                            for (int i = 0; i < currentObjects.Count; i++)
+                            {
+                                Console.WriteLine($"{i+1}. {currentObjects[i].Name}");
+                            }
+                            Console.WriteLine("----------------------------------");
+                            Console.Write("어떤 것을 조사해볼까?  (0: 뒤로가기) ");
+                            if (int.TryParse(Console.ReadLine(), out int inputNum))
+                            {
+                                if (inputNum == 0) break;
+
+                                int targetIndex = inputNum - 1;
+                                if (targetIndex >= 0 && targetIndex < currentObjects.Count)
+                                {
+                                    Console.Clear();
+                                    currentObjects[targetIndex].Interact(Player);
+                                    break;
+                                }
+                                Console.WriteLine("목록에 없는 번호입니다.");
+                                return;
+                            }
+                            Console.WriteLine("숫자만 입력해 주세요.");
+                        }
+                        break;
+                    case 0:
+                        ChangeState(GameState.Exit);
+                        IsRunning = false;
+                        return;
+                }
+                Console.WriteLine("\n(계속하려면 아무 키나 누르세요)");
+                Console.ReadKey();
+            }       
+        }
+
+        public void ExitGame()
+        {
+
+        }
+
+        public void MoveRoom(string dir)
+        {
+            int dirToInt = 0;
+            if (dir == "left") { dirToInt = 1; }
+            else if (dir == "right") { dirToInt = 2; }
             // 순환형 이동로직 선택
-            int nextIndex = (dir == 1) ?  mCurrentRoomIndex - 1 : mCurrentRoomIndex + 1;
-            if (nextIndex > 3) nextIndex = 0;
-            else if (nextIndex < 0) nextIndex = 3;
+            mCurrentRoomIndex = (dirToInt == 1) ? mCurrentRoomIndex - 1 : mCurrentRoomIndex + 1;
+            if (mCurrentRoomIndex > 3) mCurrentRoomIndex = 0;
+            else if (mCurrentRoomIndex < 0) mCurrentRoomIndex = 3;
         }
     }
 
@@ -165,7 +269,10 @@ namespace CSharp_First
     {
         static void Main(string[] args)
         {
-            
+            GameManager gm = new GameManager();
+            gm.InitGame();
+
+
         }
     }
 }
