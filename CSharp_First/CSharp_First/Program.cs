3985c3973bcdc13be1fb38e6002aa8a73c199db5
iThing1 <r930324@gmail.com>
2026-04-01 16:30:07 +0900

    [Add_Class]상호작용 오브젝트
    -상호작용 가능한 오브젝트의 공통데이터와 기능을 담은 객체
    -abstract를 붙여 물건을 만들때 반드시 데이터의 형식과 기능을 구현하도록 함

--- CSharp_First/CSharp_First/Program.cs
+++ CSharp_First/CSharp_First/Program.cs
@@ -2,10 +2,30 @@
 
 namespace CSharp_First
 {
+    public abstract class InteractableObject
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int ReqItemID { get; private set; } 
+        public int DropItemID { get; private set; }
+        public bool IsLocked { get; set; }
+
+        protected InteractableObject(string name, string desc, int reqId = 0, int dropId = 0, bool locked = false)
+        {
+            Name = name;
+            Description = desc;
+            ReqItemID = reqId;
+            DropItemID = dropId;
+            IsLocked = locked;
+        }
+
+        public abstract void Interact();
+    }    
+
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {    
 
         }
     }
