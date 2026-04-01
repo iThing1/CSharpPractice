08b220f605b48cc470cd9b4fed6cdec9e7a93ae3
iThing1 <r930324@gmail.com>
2026-04-01 18:01:21 +0900

    [update]인터페이스 설계
    -각 오브젝트들의 기능을 쉽게 탈부착하기 위해 인터페이스를 사용
    -예시로 SafeBox 클래스를 만듬. 여기에 필요한 ItemData와 메서드 구현

--- CSharp_First/CSharp_First/Program.cs
+++ CSharp_First/CSharp_First/Program.cs
@@ -1,8 +1,32 @@
 ﻿using System;
-using System.Collections.Specialized;
 
 namespace CSharp_First
 {
+    public enum ItemData
+    {
+        None = 0,
+        OldKey = 100,
+        Knife = 200,
+        Note = 300,
+    }
+
+    public class Item
+    {
+        public string GetItemName(ItemData item)
+        {
+            switch (item)
+            {
+                case ItemData.OldKey: 
+                    return "낡은 열쇠";
+                case ItemData.Knife: 
+                    return "칼";
+                case ItemData.Note:
+                    return "비밀번호가 적힌 쪽지";
+                default:
+                    return "None";
+            }
+        }
+    }
     public abstract class InteractableObject
     {
         public string Name { get; protected set; }
@@ -16,6 +40,7 @@ namespace CSharp_First
 
         public abstract void Interact();
     }
+
     public class Room
     {
         public string Name { get; private set; }
@@ -24,6 +49,61 @@ namespace CSharp_First
         public Room(string name)
         {
             Name = name;
+        }
+    }
+
+    public interface IItemDropable
+    {
+        public void DropItem();
+    }
+
+    public interface ILockable
+    {
+        public void Unlock();
+    }
+
+    public class SafeBox : InteractableObject, IItemDropable, ILockable
+    {
+        public ItemData ReqItemID { get; private set; }
+        public ItemData DropItemID { get; private set; }
+        public bool IsLocked { get; private set; } = true;
+
+        private Item itemMgr = new Item();
+
+        public SafeBox() : base ("금고", "단단히 잠겨져있는 금고이다.")
+        {
+            ReqItemID = ItemData.OldKey;
+            DropItemID = ItemData.Note;
+        }
+
+        public override void Interact()
+        {
+            Console.WriteLine($"[{Name}]");
+            Console.WriteLine(Description);
+            if (IsLocked)
+            {
+                Console.WriteLine($"{itemMgr.GetItemName(ReqItemID)}가 필요할 것 같다.");
+                Unlock();
+                return;
+            }
+            Console.WriteLine("이미 열려있는 금고이다.");
+
+        }
+        public void Unlock()
+        {
+            if (IsLocked)
+            {
+                IsLocked = false;
+                Console.WriteLine("덜컥! 금고가 열렸습니다.");
+
+                DropItem();
+            }
+        }
+
+        public void DropItem()
+        {
+            string droppedItemName = itemMgr.GetItemName(DropItemID);
+            Console.WriteLine($"금고 안에서 [{droppedItemName}]을(를) 발견했습니다!");
         }
     }
 
