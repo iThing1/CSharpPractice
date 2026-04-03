c91ec02f00b82cb26f13a0decdecab806398f41c
iThing1 <r930324@gmail.com>
2026-04-03 18:25:09 +0900

    [update]특정 조건에 따른 크리쳐 분류 메서드
    -특정 조건에 부합하는 크리쳐가 현재 파티내에 있는지를 찾아주는 메서드를 오버로딩하여 구현하였다.
    =>각각의 조건을 입력하면 거기에 해당하는 크리쳐를 보여주도록 구현하였다.
    -전체 크리쳐 목록을 볼 수 있는 ShowCreaturePool을 만들었다.

--- CSharp_First/CSharp_First/Program.cs
+++ CSharp_First/CSharp_First/Program.cs
@@ -81,16 +81,26 @@ namespace CSharp_First
     public class GameManager
     {
         private int _creatureCount = 2;
+        public List<FantasticCreature> CreaturePool = new List<FantasticCreature>();
         public List<FantasticCreature> PartyList = new List<FantasticCreature>();
         
+        public GameManager()
+        {
+            CreaturePool.Add(new GoldenGoblin());
+            CreaturePool.Add(new CrystalDragon());
+        }
+
         public void JoinRandomCreatures(int number)
         {
-            Random randNum = new Random();
-            
+            Console.Clear();
+            Console.WriteLine("=======크리쳐 랜덤 뽑기!!========");
+            Random randNum = new Random();        
             for (int i = 0; i < number; i++)
             {
+                Console.WriteLine($"{i + 1}회차 뽑기는....");
+                Thread.Sleep(2000);
                 int choice = randNum.Next(0, _creatureCount);
-                FantasticCreature newCreature = null;
+                FantasticCreature? newCreature = null;
                 switch(choice)
                 {
                     case 0:
@@ -109,15 +119,72 @@ namespace CSharp_First
             }      
         }
 
-        public void DisplayCreatureInfo(List<FantasticCreature> list)
+        public void JoinSpecificCreature(FantasticCreature creature)
+        {     
+            Console.WriteLine("=======크리쳐 선택권 사용!!========");
+            PartyList.Add(creature);
+            Console.WriteLine($"[System] {creature.Name}이(가) 파티에 합류했습니다!");
+        }
+
+        public void ShowCreaturePool()
         {
-            Console.WriteLine("\n==========<< 몬스터 정보 >>==========");
+            Console.WriteLine("==========<< 전체 크리쳐 목록 >>==========");
+            for (int i = 0; i < CreaturePool.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] Name: {CreaturePool[i].Name}\nRank: {CreaturePool[i].Rank} | Level: {CreaturePool[i].Level}\n");
+            }
+        }
+
+        public void ShowPartyInfo(List<FantasticCreature> list)
+        {
+            Console.WriteLine("==========<< 파티 정보 >>==========");
             for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine($"[{i + 1}] Name: {list[i].Name}\nRank: {list[i].Rank} | Level: {list[i].Level}\n");
             }
         }
 
+        public void ShowCreatureInfo(FantasticCreature creature)
+        {
+            Console.WriteLine($"==========<< 크리쳐 정보 >>==========");
+            Console.WriteLine($"Name: {creature.Name}\nRank: {creature.Rank} | Level: {creature.Level}\n");
+        }
+
+        public void FindSpecificCreature(string name)
+        {
+            FantasticCreature? creature = PartyList.Find(c => c.Name == name);
+            if (creature != null)
+            {
+                Console.WriteLine($"[System] {name}이(가) 파티에 존재합니다!");
+                ShowCreatureInfo(creature);
+                return;
+            }
+            Console.WriteLine($"[System] {name}이(가) 파티에 존재하지 않습니다.");
+        }
+
+        public void FindSpecificCreature(CreatureRank rank)
+        {
+            FantasticCreature? creature = PartyList.Find(c => c.Rank == rank);
+            if (creature != null)
+            {
+                Console.WriteLine($"[System] {rank}이(가) 파티에 존재합니다!");
+                ShowCreatureInfo(creature);
+                return;
+            }
+            Console.WriteLine($"[System] {rank}이(가) 파티에 존재하지 않습니다.");
+        }
+
+        public void FindSpecificCreature(int level)
+        {
+            FantasticCreature? creature = PartyList.Find(c => c.Level >= level);
+            if (creature != null)
+            {
+                Console.WriteLine($"[System] 레벨 {level}이상인 몬스터가 파티에 존재합니다!");
+                ShowCreatureInfo(creature);
+                return;
+            }
+            Console.WriteLine($"[System] 레벨 {level}이상인 몬스터가 파티에 존재하지 않습니다.");
+        }
     }
 
     internal class Program
@@ -125,11 +192,6 @@ namespace CSharp_First
         static void Main(string[] args)
         {   
             GameManager gm = new GameManager();
-            gm.JoinRandomCreatures(3);
-            gm.DisplayCreatureInfo(gm.PartyList);
-
-            gm.PartyList[0].LevelUp();
-            gm.DisplayCreatureInfo(gm.PartyList);
         }
     }
 }
