defab76595545a7036fadf6f9589d9d708b63744
iThing1 <r930324@gmail.com>
2026-04-03 16:32:20 +0900

    [Add_Class&Enum]몬스터 등급enum과 부모클래스, 자식클래스 설계
    -부모클래스 FantasticCreature과 이를 상속받는 GoldenGoblin을 설계하였다.
    -LevelUp은 Creature의 공통로직을 부모에서 구현하고, 자식마다 다른 로직은 자식클래스에서 각자 구현하도록 virtual로 선언하였다.

--- CSharp_First/CSharp_First/Program.cs
+++ CSharp_First/CSharp_First/Program.cs
@@ -2,11 +2,60 @@
 
 namespace CSharp_First
 {
-    internal class Program
+    public enum CreatureRank
     {
+        None,
+        Normal,
+        Rare,
+        Unique,
+        Legendary,
+        Ancient,
+    }
+
+    public class FantasticCreature
+    {
+        public string Name { get; protected set; } = "";
+        public CreatureRank Rank { get; protected set; }
+        public int Level { get; protected set; }
+
+        public void DisplayCreatureInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Rank: {Rank}");
+            Console.WriteLine($"Level: {Level}");
+        }
+
+        public virtual void LevelUp()
+        {
+            Level++;
+            Console.WriteLine($"[{Name}]의 레벨이 올라 {Level}이 되었다!");
+        }
+
+    }
+
+    public class GoldenGoblin : FantasticCreature
+    {
+        public GoldenGoblin()
+        {
+            Name = "황금 고블린";
+            Rank = CreatureRank.Normal;
+            Level = 1;
+        }
+
+        public override void LevelUp()
+        {
+            base.LevelUp();
+            Console.WriteLine($"[{Name}]의 황금빛이 더욱 빛나기 시작했다!");
+        }
+    }
+
+    internal class Program
+    { 
         static void Main(string[] args)
         {
-
+            FantasticCreature goblin = new GoldenGoblin();
+            goblin.DisplayCreatureInfo();
+            goblin.LevelUp();
         }
     }
 }
