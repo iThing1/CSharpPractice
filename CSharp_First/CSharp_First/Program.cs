dd5a4a8c4c41f0b22e5a32a385836acb54ead5fc
iThing1 <r930324@gmail.com>
2026-04-03 17:55:04 +0900

    [Add_Class]크리쳐: 크리스탈 드래곤, 게임 매니저 설계
    -새로운 크리쳐 생성. LevelUp로직을 특징에 맞게 수정하고, 고유의 기능을 구현하였다.
    -GameManager 클래스: 게임 진행에 필요한 기능을 여기에 구현하였다.
    => 크리쳐파티 구성(List 활용), UI출력, 랜덤 크리쳐 합류(random 사용)
    -테스트를 위해 Main로직 변경 및 어색한 텍스트 출력문 수정

--- CSharp_First/CSharp_First/Program.cs
+++ CSharp_First/CSharp_First/Program.cs
@@ -2,6 +2,7 @@
 
 namespace CSharp_First
 {
+    
     public enum CreatureRank
     {
         None,
@@ -18,16 +19,12 @@ namespace CSharp_First
         public CreatureRank Rank { get; protected set; }
         public int Level { get; protected set; }
 
-        public void DisplayCreatureInfo()
-        {
-            Console.WriteLine($"Name: {Name}");
-            Console.WriteLine($"Rank: {Rank}");
-            Console.WriteLine($"Level: {Level}");
-        }
-
         public virtual void LevelUp()
         {
             Level++;
+            Console.WriteLine($"====================================");
+            Console.WriteLine($"         !!! Level Up !!!            ");
+            Console.WriteLine($"====================================");
             Console.WriteLine($"[{Name}]의 레벨이 올라 {Level}이 되었다!");
         }
 
@@ -49,13 +46,90 @@ namespace CSharp_First
         }
     }
 
-    internal class Program
-    { 
-        static void Main(string[] args)
+    public class CrystalDragon : FantasticCreature
+    {
+        public int CrystalCount { get; private set; }
+        public CrystalDragon()
         {
-            FantasticCreature goblin = new GoldenGoblin();
-            goblin.DisplayCreatureInfo();
-            goblin.LevelUp();
+            Name = "크리스탈 드래곤";
+            Rank = CreatureRank.Legendary;
+            Level = 5;
+            CrystalCount = 0;
+        }
+        public override void LevelUp()
+        {
+            base.LevelUp();
+            CrystalCount += Level;
+            Console.WriteLine($"[{Name}]에게서 크리스탈이 떨어져 나온다!!");
+            DropCrystal();
+        }
+
+        public void Roar()
+        {
+             Console.WriteLine($"[{Name}]이(가) 포효했다! 주변이 진동한다!");
+        }
+
+        public void DropCrystal()
+        {
+            if (CrystalCount > 0)
+            {
+                Console.WriteLine($"[System]크리스탈이 {CrystalCount}개 떨어졌습니다.");
+            }
+        }
+    }
+
+    public class GameManager
+    {
+        private int _creatureCount = 2;
+        public List<FantasticCreature> PartyList = new List<FantasticCreature>();
+        
+        public void JoinRandomCreatures(int number)
+        {
+            Random randNum = new Random();
+            
+            for (int i = 0; i < number; i++)
+            {
+                int choice = randNum.Next(0, _creatureCount);
+                FantasticCreature newCreature = null;
+                switch(choice)
+                {
+                    case 0:
+                        newCreature = new GoldenGoblin();
+                        break;
+                    case 1:
+                        newCreature = new CrystalDragon();
+                        break;
+                }
+
+                if (newCreature != null)
+                {
+                    PartyList.Add(newCreature);
+                    Console.WriteLine($"[System] {newCreature.Name}이(가) 파티에 합류했습니다!");
+                }         
+            }      
+        }
+
+        public void DisplayCreatureInfo(List<FantasticCreature> list)
+        {
+            Console.WriteLine("\n==========<< 몬스터 정보 >>==========");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] Name: {list[i].Name}\nRank: {list[i].Rank} | Level: {list[i].Level}\n");
+            }
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            GameManager gm = new GameManager();
+            gm.JoinRandomCreatures(3);
+            gm.DisplayCreatureInfo(gm.PartyList);
+
+            gm.PartyList[0].LevelUp();
+            gm.DisplayCreatureInfo(gm.PartyList);
         }
     }
 }
